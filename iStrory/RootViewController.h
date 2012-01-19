89a065d8bfcca9f74db572f7fff15c5ba7d30719
Mac <mahmood1@Mahmood1s-Mac.local>
2012-01-19 06:59:35 +0100

    Initial Commit

---
+++ iStrory/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  iStrory
+//
+//  Created by Mac on 1/19/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
